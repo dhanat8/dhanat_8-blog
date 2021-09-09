601704415e9a1f9740094615f71291c88bbf2ca9
dhanat8 <88369216+dhanat8@users.noreply.github.com>
2021-09-09 16:24:34 +0700

    add email validator back

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL,Email
 from flask_ckeditor import CKEditorField
 
 
@@ -14,14 +14,14 @@ class CreatePostForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(),Email(allow_empty_local=False)])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email",  validators=[DataRequired(),Email(allow_empty_local=False)])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
 
